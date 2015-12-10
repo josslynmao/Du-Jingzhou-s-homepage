af6816e0ad8be890c9449bd1b53d8c88a69da92e
josslynmao <mao.huifeng@hotmail.com>
2015-12-10 19:21:14 +0800

    xxx

--- static/js/gallery.js
+++
@@ -1,9 +0,0 @@
-function showPic(whichpic)
-{
-	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	placeholder.setAttribute("src",source);
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	description.childNodes[0].nodeValue = text;
-}
